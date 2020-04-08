03ed00a4ae3808dd97adf7645672f419869ac752
Ubuntu <ubuntu@ip-172-31-48-227.ec2.internal>
2020-04-08 23:04:47 +0000

    Agregar sg.tf

---
+++ sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "Bootcamp-Demo"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Demo-Bootcamp"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
